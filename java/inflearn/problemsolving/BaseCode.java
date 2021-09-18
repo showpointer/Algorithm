004519f65186807d2a44d39547a6d1584f302157
wanho.im <wanho92@gmail.com>
2021-09-18 14:49:43 +0900

    add: java base code

---
+++ java/inflearn/problemsolving/BaseCode.java
@@ -0,0 +1,20 @@
+package inflearn.problemsolving;
+
+import java.util.Scanner;
+
+public class BaseCode {
+
+    public static String solve(String s) {
+        String answer = "";
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        // g0en2T0s8eSoft => 208
+        Scanner in=new Scanner(System.in);
+        String input = in.nextLine();
+
+        System.out.println(solve(input));
+    }
+}
